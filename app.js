c4edf493c27d8543c271bcf3c339a7a4c2827129
Terrence Ng <tng305@ucsc.edu>
2023-06-27 20:35:27 -0700

    changes active button but stops working after a few button clicks

--- app.js
+++ app.js
@@ -16,7 +16,22 @@ function PageTransitions() {
 
     //Sections Active Class
     allSections[0].addEventListener('click', (e) => {
-        console.log(e.target);
+        const id = e.target.dataset.id;
+        if(id) {
+            //remove selected from the other btns
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //hide other sections
+            sections.forEach((section) => {
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
     })
 }
 
